d76bb6d479b36dc061ec70ada3f8925ac80de6ca
Arvid Brunsell <arvid432000@gmail.com>
2022-07-22 17:03:50 -0400

    Added analyzer decibel marks
    cleaned up code

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -79,11 +79,6 @@ void RotarySliderWithLabels::paint(juce::Graphics& g)
 
     auto sliderBounds = getSliderBounds();
 
-    /*g.setColour(Colours::red);
-    g.drawRect(getLocalBounds());
-    g.setColour(Colours::yellow);
-    g.drawRect(sliderBounds);*/
-
     getLookAndFeel().drawRotarySlider(g, 
         sliderBounds.getX(), 
         sliderBounds.getY(), 
@@ -233,7 +228,7 @@ void ResponseCurveComponent::paint(juce::Graphics& g)
 
     g.drawImage(background, getLocalBounds().toFloat());
 
-    auto responseArea = getAnalysisArea(); // getRenderArea();
+    auto responseArea = getAnalysisArea();
 
     auto w = responseArea.getWidth();
 
@@ -308,9 +303,9 @@ void ResponseCurveComponent::resized()
 
     Array<float> freqs
     {
-        20, /*30, 40,*/ 50, 100,
-        200, /*300, 400,*/ 500, 1000,
-        2000, /*3000, 4000,*/ 5000, 10000,
+        20, 50, 100,
+        200, 500, 1000,
+        2000, 5000, 10000,
         20000
     };
 
@@ -330,9 +325,6 @@ void ResponseCurveComponent::resized()
 
     g.setColour(Colours::dimgrey);
     for (auto x : xs) {
-        //auto normX = mapFromLog10(f, 20.f, 20000.f);
-
-        //   g.drawVerticalLine(getWidth() * normX, 0.f, getHeight());
         g.drawVerticalLine(x, top, bottom);
     }
 
@@ -344,12 +336,9 @@ void ResponseCurveComponent::resized()
     for (auto gDb : gain)
     {
         auto y = jmap(gDb, -24.f, 24.f, float(bottom), float(top));
-        //g.drawHorizontalLine(y, 0, getWidth());
         g.setColour(gDb == 0.f ? Colour(0u, 172u, 1u) : Colours::darkgrey);
         g.drawHorizontalLine(y, left, right);
     }
-
-    // g.drawRect(getAnalysisArea());
 
     g.setColour(Colours::lightgrey);
     const int fontHeight = 10;
@@ -403,6 +392,15 @@ void ResponseCurveComponent::resized()
         
         g.setColour(gDb == 0.f ? Colour(0u, 172u, 1u) : Colours::lightgrey);
 
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
+
+        str.clear();
+        str << (gDb - 24.f);
+
+        r.setX(1);
+        textWidth = g.getCurrentFont().getStringWidth(str);
+        r.setSize(textWidth, fontHeight);
+        g.setColour(Colours::lightgrey);
         g.drawFittedText(str, r, juce::Justification::centred, 1);
     }
 
